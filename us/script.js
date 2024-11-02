9c0619d00e5cf4364e24b40b667081a7c8e48c23
TayyabM553 <tm351110@gmail.com>
2024-11-02 11:54:39 +0500

    Add files via upload

---
+++ us/script.js
@@ -0,0 +1,7 @@
+// Example: Adding interactivity for "Add to Cart" button
+document.querySelectorAll('.product-card button').forEach(button => {
+    button.addEventListener('click', () => {
+      alert("Product added to cart!");
+    });
+  });
+  
